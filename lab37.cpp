a405f7271be0dc1ced78b09940d718132a944dd0
arayapudi626 <rayapudianisha@gmail.com>
2025-12-06 22:06:07 -0800

    milestone 1

--- lab37.cpp
+++ lab37.cpp
@@ -1,5 +1,14 @@
 #include <iostream>
+#include <string>
 using namespace std;
+int sum_ascii(const std::string &s){
+    int sum = 0;
+    for (char c : s){
+        sum+=(int)c;
+    }
+    return sum;
+}
+
 int main() {
 char a = 'A';
 cout << a << endl;
@@ -7,6 +16,9 @@ cout << (int) a << endl;
 int b = 66;
 cout << b << endl;
 cout << (char) b << endl;
+
+string test = "hello";
+cout << "sum test" << test << sum_ascii(test) << endl;
 return 0;
 }
 /*
