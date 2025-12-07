58ef0e761107661522bbe4e6a0de9acb9c5afea4
arayapudi626 <rayapudianisha@gmail.com>
2025-12-06 22:45:39 -0800

    update

--- lab37.cpp
+++ lab37.cpp
@@ -14,7 +14,12 @@ int gen_hash_index(const std::string &s){
 }
 
 int main() {
+    cout << "test" << endl;
 ifstream fin("lab-37-data-2.txt");
+if (!fin.is_open()) {
+    cerr << "could not open file\n";
+    return 1;
+}
 string text;
 long total = 0;
 while (fin >> text){
@@ -23,6 +28,7 @@ while (fin >> text){
 cout << "Total ASCII sum = " << total << endl;
 
 fin.clear();
+fin.seekg(0); 
 map<int, list<string>> hash_table;
 while (fin >> text){
     int hash_index = gen_hash_index(text);
@@ -34,7 +40,7 @@ cout << "First 100 entries: " << endl;
 int count = 0;
 
     for (const auto val: hash_table){
-        if (count >= 100) break;
+        if (count >= 10) break;
         cout << "hash index " << val.first << ": ";
         for (const string &s : val.second) {
             cout << s << " " << endl;
